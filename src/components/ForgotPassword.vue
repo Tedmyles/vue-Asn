0f7bbbfeb112153d44169067f9f16cca928c22b4
Tedmyles <mylesteddy7@gmail.com>
2024-03-14 10:00:26 +0300

    forgot-password

---
+++ src/components/ForgotPassword.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h2>Forgot Password</h2>
+      <form @submit.prevent="forgotPassword">
+        <input type="email" v-model="email" placeholder="Email" required>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        email: ''
+      };
+    },
+    methods: {
+      forgotPassword() {
+        axios.post('http://your-api.com/forgot-password', {
+          email: this.email
+        })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      }
+    }
+  }
+  </script>
+  
