ff8badb6571640623373b61d0a0df612db3b55cb
Tedmyles <mylesteddy7@gmail.com>
2024-03-14 09:52:45 +0300

    signup

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h2>Sign Up</h2>
+      <form @submit.prevent="signup">
+        <input type="text" v-model="username" placeholder="Username" required>
+        <input type="email" v-model="email" placeholder="Email" required>
+        <input type="password" v-model="password" placeholder="Password" required>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      signup() {
+        axios.post('http://your-api.com/signup', {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      }
+    }
+  }
+  </script>
+  
